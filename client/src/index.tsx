2d7d2541ba39a66e314ae5a58e5e094682e9978e
akashag55 <2017pietcsakash005@poornima.org>
2022-04-20 16:07:34 +0530

    filr added again

---
+++ client/src/index.tsx
@@ -0,0 +1,15 @@
+import App from "./Components/root/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./tailwind.output.css"
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ client/src/main.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import App from './Components/root/App';
+
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+    ,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
