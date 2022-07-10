0631f07a4a96392fa5af3561f70b3a5fdf790fa7
aashishsinghal <aashish451singhal@gmail.com>
2022-07-11 02:37:03 +0530

    Made tRPC quries and mutation for tags

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,15 +1,52 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
-import { Footer } from "./index";
+import React from 'react';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { Footer } from './index';
+import { useSession } from 'next-auth/react';
+import { trpc } from '../utils/trpc';
 
 const Results = () => {
+  // const { data: session } = useSession();
+  const tags = trpc.useQuery(['tags.getAllTags']);
+  console.log('client data - ', tags, tags.data);
+  // const { data: tags, success, isloading } = response;
+
+  const createTagMutation = trpc.useMutation(['tags.create-tag']);
+
+  const createTagHandler = () => {
+    const tag = {
+      name: 'teatTag',
+      category: ['test'],
+    };
+    createTagMutation.mutate(tag);
+  };
+
   return (
-    <Box w="80%" p={4}>
-        
-      Results
-      <Footer />
-    </Box>
+    <>
+      <Flex
+        w="80%"
+        p={4}
+        direction="column"
+        justify="space-between"
+        align="center"
+      >
+        Results
+        <Button onClick={createTagHandler}>Create Tag</Button>
+        <Flex>{tags.data && tags.data.map((tag) => <p>{tag.name}</p>)}</Flex>
+        <Footer />
+      </Flex>
+    </>
   );
 };
+
+// export const getServerSideProps = async () => {
+//   const { data: session } = useSession();
+//   const tags = trpc.useQuery(['tags.getAllTags']);
+//   console.log('server side data - ', tags);
+//   return {
+//     props: {
+//       tags,
+//     },
+//   };
+// };
 
 export default Results;
